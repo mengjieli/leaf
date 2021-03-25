45d6d1bf4db17c67828ff55472275caa0e7c3092
limengjie <limengjie@hortorgames.com>
2021-03-26 01:12:50 +0800

    添加 wxgame net

--- client/cli/copy-wx-js.sh
+++ client/cli/copy-wx-js.sh
@@ -10,6 +10,7 @@ cp ./../web/libs/mobx/mobx.min.js ./../../wxgame/js/libs/mobx.js
 cp ./../web/libs/msgpack/msgpack.min.js ./../../wxgame/js/libs/msgpack.js
 cp ./../web/libs/pako/pako.min.js ./../../wxgame/js/libs/pako.js
 cp ./../web/libs/orange/orange.js ./../../wxgame/js/libs/orange.js
-cp ./../web/libs/orange/orange.js ./../../wxgame/js/libs/orange.js
+cp ./../web/libs/orange-modules/native/native.js ./../../wxgame/js/libs/native.js
+cp ./../web/libs/orange-modules/sync-data/sync-data.js ./../../wxgame/js/libs/sync-data.js
 
 cp ./../dist/main.js ./../../wxgame/js/main.js
--- wxgame/game.js
+++ wxgame/game.js
@@ -6,11 +6,15 @@ import './js/libs/tiny2d'
 import './js/libs/mobx'
 import './js/libs/msgpack'
 import './js/libs/orange'
+import './js/libs/native'
+import './js/libs/sync-data'
 import './js/libs/behavior-tree'
 import './js/libs/tween'
 import './js/libs/symbol'
 
 import './js/main'
+
+window["IS_WEB"] = true;
 
 var fs = window.wxfs = wx.getFileSystemManager();
 window.wxloadText = function(url,onComplete) {
